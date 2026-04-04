abbd15e8144f040d29b3da61715c204922c813cf
polka <polina.fesenko.04@gmail.com>
2026-04-04 04:42:44 +0300

    the loop скопировала из фвйла подсказки

--- wp-theme-layout/index.php
+++ wp-theme-layout/index.php
@@ -1,7 +1,34 @@
 <<?php get_header(); ?>
 <div class="content-area">
     <main>
-        <p>Звоала</p>
+
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_post_thumbnail('large'); ?>
+
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+            <div class="post-meta">
+                <?php echo get_the_date(); ?>
+                <?php the_author_posts_link(); ?>
+                <?php the_category(', '); ?>
+            </div>
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php // або для анонсу: the_excerpt(); ?>
+            </div>
+        </article>
+
+    <?php endwhile; ?>
+
+    <?php the_posts_pagination(); ?>
+
+<?php else : ?>
+    <p>Постів не знайдено.</p>
+<?php endif; ?>
     </main>
     <?php get_sidebar(); ?>
 </div>
--- wp-theme-layout/single.php
+++ wp-theme-layout/single.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
+
 <div class="content-area content-area--full">
     <main class="single-post">
-    
+        Здесь будет содержимое записи
     </main>
 </div>
+
 <?php get_footer(); ?>
